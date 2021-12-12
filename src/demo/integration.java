76e303f5f00d9e72ce437196c7d98b6534f8c9a3
sumithradevi1996 <iamsumithradevi@gmail.com>
2021-12-12 19:31:08 +0530

    integrating

---
+++ src/demo/integration.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class integration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String expectedUrl= driver.getCurrentUrl();
+        xzXZXif(actualUrl.equalsIgnoreCase(expectedUrl))
+        {
+            System.out.println("Test passed");
+        } else
+        {
+            System.out.println("Test failed");
+            
+	}
+
+}
